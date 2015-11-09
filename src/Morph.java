c715ac971df6e8de01c8ca854b09b46f8d23fa66
Tossmann <trautmann.phillip@gmx.de>
2015-11-09 10:23:22 +0100

    test if changes appear

--- src/Morph.java
+++ src/Morph.java
@@ -145,9 +145,10 @@ public class Morph extends JPanel {
 	public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {       	
             public void run() {
                 createAndShowGUI();
+                System.out.println("penis");
             }
         });
 	}
